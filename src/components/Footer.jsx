55d2c9baaf1817e3dc616d0a77d0702cc8529b81
Paulo H. C. Santana <cotinguibasantana@gmail.com>
2026-07-07 21:12:51 -0300

    Correct footer text and update year to 2024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h2>Guimares Doces</h2>
-        <p>Adoçando a sua vida desde 2026.</p>
+        <h2>Guimarães Doces</h2>
+        <p>Adoçando a sua vida desde 2024.</p>
         <div className="made-with">
           Feito com <Heart size={20} weight="fill" color="var(--color-pink)" /> por Guimares
         </div>
